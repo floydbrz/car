8ffb58c62de0b7539e9847c277a9c3d8f8695046
bca <bca@Bcalab4-m18>
2023-02-20 09:50:58 +0530

    commit2

--- github131/src/github131/Car.java
+++ github131/src/github131/Car.java
@@ -3,12 +3,14 @@ package github131;
 public class Car {
 	String model;
     int speed;
+    int manuf_year;
     
-    
-    Car(String model, int speed)
+    Car(String model, int speed,int manuf_year )
     {
     	this.model=model;
     	this.speed=speed;
+    	this.manuf_year=manuf_year;
+
     }
 
 	public static void main(String[] args) {
