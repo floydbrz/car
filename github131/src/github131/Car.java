84441beb0cf40aa8f04182cb973db2be272ec56b
bca <bca@Bcalab4-m18>
2023-02-20 09:44:19 +0530

    push1togithub

---
+++ github131/src/github131/Car.java
@@ -0,0 +1,19 @@
+package github131;
+
+public class Car {
+	String model;
+    int speed;
+    
+    
+    Car(String model, int speed)
+    {
+    	this.model=model;
+    	this.speed=speed;
+    }
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
